a408c235252aa6bf4e8276b99fef085342fdcdc7
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2020-11-05 23:25:43 +0500

    homework 4.5.1 implement methods for Contacts.java

--- src/homework4_5_1/Contacts.java
+++ src/homework4_5_1/Contacts.java
@@ -1,20 +1,42 @@
 package homework4_5_1;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
-public class Contacts {
+public class Contacts implements Cloneable {
 
     Map<String, Contact> contacts = new HashMap<>();
 
-    /*
-    Добавление контакта (имя, фамилия, номер телефона, группа контакта: работа, друзья, семья).
-    Удаление контакта по имени и фамилии.
-    Поиск контакта по номеру.
-     */
+    public Map<String, Contact> addContact(String name, String surname, String number, Group group) {
+        Contact contact = new Contact();
+        contact.setName(name);
+        contact.setSurname(surname);
+        contact.setPhone(number);
+        contact.setGroup(group);
+        contacts.put(number, contact);
+        return contacts;
+    }
+
+    public void deleteContactByNameSurname(String name, String surname) {
+        Iterator<Map.Entry<String, Contact>> iterator = contacts.entrySet().iterator();
+        while (iterator.hasNext()) {
+            final Map.Entry<String, Contact> unit = iterator.next();
+            String nameValue = unit.getValue().getName();
+            String surnameValue = unit.getValue().getSurname();
+            if (nameValue.equals(name) && surnameValue.equals(surname)) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public Contact searchContactByPhoneNumber(String number) {
+        return contacts.getOrDefault(number, null);
+    }
 
     @Override
     public String toString() {
-        return "";
+        return "List of contacts is: " + Arrays.toString(contacts.entrySet().toArray());
     }
 }
--- src/homework4_5_1/Main.java
+++ src/homework4_5_1/Main.java
@@ -4,6 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Contacts contacts = new Contacts();
         MissedCalls missedCalls = new MissedCalls();
+
+        contacts.addContact("Yury", "Maslov", "+7896", Group.FAMILY);
+        contacts.addContact("Ivan", "Petrov", "+7865", Group.FRIENDS);
+        contacts.addContact("Irina", "Maslova", "+7874", Group.WORK);
+
+        contacts.deleteContactByNameSurname("Ivan", "Petrov");
+        Contact searchableContact = contacts.searchContactByPhoneNumber("+7874");
+        System.out.println(contacts);
+        System.out.println(searchableContact);
     }
 
     /*
